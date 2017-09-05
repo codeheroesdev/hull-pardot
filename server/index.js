a9538f19da2e5816c206515284cb7cbe6fbc5e33
Michal Raczka <michal@raczka.me>
2017-09-05 13:51:03 +0200

    Adds redis cache

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import Hull, { Connector } from "hull";
 import { Cache } from "hull/lib/infra";
+import redisStore from "cache-manager-redis";
 
 import server from "./server";
 
@@ -8,10 +9,22 @@ if (process.env.LOG_LEVEL) {
   Hull.logger.transports.console.level = process.env.LOG_LEVEL;
 }
 
-const cache = new Cache({
-  store: "memory",
-  ttl: 86400
-});
+let cache
+
+
+if (process.env.REDIS_URL) {
+    cache = new Cache({
+      store: redisStore,
+      url: process.env.REDIS_URL,
+      ttl: 86400
+    });
+} else {
+    cache = new Cache({
+      store: "memory",
+      ttl: 86400
+    });
+}
+
 
 const connector = new Connector({
   hostSecret: process.env.SECRET || "1234",
